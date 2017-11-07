041d750e884b2c93664e4928fd0a9c1fbd9cf16b
C0deMunk33 <hollandcodeandgraphics@gmail.com>
2017-11-07 11:52:50 -0700

    Add files via upload

---
+++ EtherealFoundationOwned.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.18;
+
+contract EtherealFoundationOwned {
+	address private Owner;
+    
+	function IsOwner(address addr) view public returns(bool)
+	{
+	    return Owner == addr;
+	}
+	
+	function TransferOwner(address newOwner) public onlyOwner
+	{
+	    Owner = newOwner;
+	}
+	
+	function EtherealFoundationOwned() public
+	{
+	    Owner = msg.sender;
+	}
+	
+	function Terminate() public onlyOwner
+	{
+	    selfdestruct(Owner);
+	}
+	
+	modifier onlyOwner(){
+        require(msg.sender == Owner);
+        _;
+    }
+}
