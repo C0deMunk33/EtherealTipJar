3e18444503533ce68339af5e80d60c66405c765b
C0deMunk33 <hollandcodeandgraphics@gmail.com>
2017-11-07 11:51:29 -0700

    Update EtherealTipJar.sol

--- EtherealTipJar.sol
+++ EtherealTipJar.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.4.18;
 import "./EtherealFoundationOwned.sol";
+import "./EtherealToken.sol";
 
 contract EtherealTipJar  is EtherealFoundationOwned{
     string public constant CONTRACT_NAME = "EtherealTipJar";
-    string public constant CONTRACT_VERSION = "A";
+    string public constant CONTRACT_VERSION = "B";
     string public constant QUOTE = "'The universe never did make sense; I suspect it was built on government contract.' -Robert A. Heinlein";
     
     
@@ -20,6 +21,18 @@ contract EtherealTipJar  is EtherealFoundationOwned{
 		{
 			to.transfer(value);
 			TransferedEth(to, value);
+		}   
+	}
+    
+    event TransferedERC20(address tokenContract, address indexed to, uint256 value);
+	function TransferERC20(address tokenContract, address to, uint256 value) public onlyOwner{
+	    
+	    EtherealToken token = EtherealToken(tokenContract);
+	    
+        if(value > 0)
+		{
+			token.transfer(to, value);
+			TransferedERC20(tokenContract,to, value);
 		}   
 	}
 }
